65f733970be6492316f67918108c85a0871cd998
William Moehlig <32701643+WilliamMoehlig@users.noreply.github.com>
2019-10-16 11:43:29 +0200

    Testing React router (#1)

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './modules/home/home';
+import Login from './modules/login/login';
+import NotFound from './modules/pages/notFound';
+
+export function AppWithoutRouter() {
+  return (
+    <Switch>
+      <Route component={Login} path="/login" />
+      <Route component={Home} path="/" />
+      <Route component={NotFound} path="*" />
+    </Switch>
+  );
+}
 
 export function App() {
   return (
-    <>
-      <h1>Hello from ES2015+</h1>
-      <div className="main container-fluid" />
-    </>
+    <Router>
+      <AppWithoutRouter />
+    </Router>
   );
 }
 
---
+++ src/app.spec.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import RenderWithRouter from '../test/renderWithRouter';
+import '@testing-library/jest-dom/extend-expect';
+import { AppWithoutRouter } from './app';
+
+jest.mock('./modules/home/home', () => () => {
+  return <h1 data-testid="HomeMock">Mocked Home</h1>;
+});
+jest.mock('./modules/login/login', () => () => {
+  return <h1 data-testid="LoginMock">Mocked Login</h1>;
+});
+jest.mock('./modules/pages/notFound', () => () => {
+  return <h1 data-testid="NotFoundMock">Mocked NotFound</h1>;
+});
+
+describe('App routing', () => {
+  test('it should route to notFound', () => {
+    // Act
+    const { getByTestId, queryByTestId } = RenderWithRouter(<AppWithoutRouter />, { route: 'fs' });
+    // Assert;
+    getByTestId('NotFoundMock');
+    expect(queryByTestId('LoginMock')).not.toBeInTheDocument();
+    expect(queryByTestId('HomeMock')).not.toBeInTheDocument();
+  });
+  test('it should route to home', () => {
+    // Act
+    const { getByTestId, queryByTestId } = RenderWithRouter(<AppWithoutRouter />, { route: '/' });
+    // Assert;
+    getByTestId('HomeMock');
+    expect(queryByTestId('LoginMock')).not.toBeInTheDocument();
+    expect(queryByTestId('NotFoundMock')).not.toBeInTheDocument();
+  });
+  test('it should route to login', () => {
+    // Act
+    const { getByTestId, queryByTestId } = RenderWithRouter(<AppWithoutRouter />, { route: '/login' });
+    // Assert;
+    getByTestId('LoginMock');
+    expect(queryByTestId('NotFoundMock')).not.toBeInTheDocument();
+    expect(queryByTestId('HomeMock')).not.toBeInTheDocument();
+  });
+});
