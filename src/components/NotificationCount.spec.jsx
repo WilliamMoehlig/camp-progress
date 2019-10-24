79ace668776251433f8ff49e6c144ebccab17449
William Moehlig <william.moehlig@hotmail.com>
2019-10-24 11:12:54 +0200

    Finishing test on NotificationCounter

--- src/components/NotificationCount.spec.jsx
+++ src/components/NotificationCount.spec.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import renderWithRedux from '../../test/renderWithRedux';
 import NotificationCount from './NotificationCount';
+import identityContext from '../contexts/IdentityContext';
+import notification from '../store/notification/notification';
 
 describe('test Notification component', () => {
-  function render(initialState = {}) {
-    return renderWithRedux(<NotificationCount />, initialState);
+  function render(initialState = {}, currentIdentity = undefined) {
+    return renderWithRedux(
+      <identityContext.Provider value={{ name: currentIdentity, setIdentity: () => {} }}>
+        <NotificationCount />
+      </identityContext.Provider>,
+      { initialState }
+    );
   }
 
   test('it should be the right image', () => {
@@ -17,5 +24,18 @@ describe('test Notification component', () => {
     findByText('(0)');
   });
 
-  test('it should show the length of the current notification array of the user ', () => {});
+  test('it should show the length of the current notification array of the user ', () => {
+    const notification1 = notification('tom', 'admin', 'my first notification', '2019-05-12');
+    const notification2 = notification('admin', 'tom', 'my second notification', '2019-05-12');
+
+    const initialState = {
+      notifications: {
+        [notification1.to]: [notification1],
+        [notification2.to]: [notification2],
+      },
+    };
+    const { findByText } = render(initialState, 'admin');
+
+    findByText('(1)');
+  });
 });
